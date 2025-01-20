f33e542fdc8c8891f7c4e18b700ec702cd56101d
WilsonLimSet <wilsonlimsetiawan@gmail.com>
2025-01-20 01:52:29 +0000

    analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -41,8 +41,8 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
+        <Analytics />
       </body>
-      <Analytics />
     </html>
   );
 }
